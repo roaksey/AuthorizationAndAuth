889172b38bbb2796b225b78f27479892abd807ae
RajeevKumarMahato <rajeev@teksewa.com>
2023-07-10 17:37:29 +0545

    setup Mailjet Email Sender

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -11,9 +11,9 @@ namespace IdentityManager.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
-        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-            _userManager = userManager; 
+            _userManager = userManager;
             _signInManager = signInManager;
         }
         public IActionResult Index()
@@ -22,7 +22,7 @@ namespace IdentityManager.Controllers
         }
 
         [HttpGet]
-        public IActionResult Register(string returnUrl=null)
+        public IActionResult Register(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
             RegisterViewModel registerViewModel = new RegisterViewModel();
@@ -39,7 +39,8 @@ namespace IdentityManager.Controllers
             {
                 var userManager = new ApplicationUser() { Name = model.Name, Email = model.Email, UserName = model.Email };
                 var result = await _userManager.CreateAsync(userManager, model.Password);
-                if(result.Succeeded) { 
+                if (result.Succeeded)
+                {
                     await _signInManager.SignInAsync(userManager, isPersistent: false);
                     return LocalRedirect(returnUrl);
                 }
@@ -48,7 +49,7 @@ namespace IdentityManager.Controllers
             return View(model);
         }
         [HttpGet]
-        public IActionResult Login(string returnUrl=null)
+        public IActionResult Login(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
             return View();
@@ -62,7 +63,7 @@ namespace IdentityManager.Controllers
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,lockoutOnFailure:true);
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
                 if (result.IsLockedOut)
                 {
                     return View("Lockout");
@@ -70,7 +71,7 @@ namespace IdentityManager.Controllers
                 if (result.Succeeded)
                 {
                     return LocalRedirect(returnUrl);
-                }               
+                }
                 else
                 {
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");
@@ -79,12 +80,27 @@ namespace IdentityManager.Controllers
             }
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+
+            return View(model);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LogOff()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction(nameof(HomeController.Index), "Home");   
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
         private void AddErrors(IdentityResult result)
@@ -96,3 +112,5 @@ namespace IdentityManager.Controllers
         }
     }
 }
+
+   
---
+++ Service/MailJetEmailSender.cs
@@ -0,0 +1,79 @@
+﻿
+using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Service
+{
+    public class MailJetEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _config;
+        public MailJetOptions mailJetOptions;
+        public MailJetEmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            mailJetOptions = _config.GetSection("MailJet").Get<MailJetOptions>();
+            MailjetClient client = new MailjetClient(mailJetOptions.ApiKey, mailJetOptions.SecretKey);
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "roaksey@proton.me"},
+        {"Name", "Rajeev"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          "roaksey@proton.me"
+         }, {
+          "Name",
+          "Rajeev"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      }, {
+       "TextPart",
+       htmlMessage
+      }, {
+       "HTMLPart",
+       "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!"
+      }, {
+       "CustomID",
+       "AppGettingStartedTest"
+      }
+     }
+             });
+         await client.PostAsync(request);
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
+            //    Console.WriteLine(response.GetData());
+            //}
+            //else
+            //{
+            //    Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+            //    Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+            //    Console.WriteLine(response.GetData());
+            //    Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+            //}
+        }
+    }
+}
